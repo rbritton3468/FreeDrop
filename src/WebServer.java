aea904d95cb9b6660949180ae09d52c0fc0d952d
rbritton3468 <44485771+rbritton3468@users.noreply.github.com>
2025-03-24 16:37:51 -0500

    get job from request

--- src/WebServer.java
+++ src/WebServer.java
@@ -1,34 +1,72 @@
 import java.io.*;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.http.HttpHeaders;
+import java.net.http.HttpResponse;
 import java.util.Arrays;
+
+
 
 
 public class WebServer {
     private ServerSocket server;
     public void acceptConnection(int port) throws IOException  {
-        server = new ServerSocket(port);
+        ServerSocket server = new ServerSocket(port);
         Socket socket = server.accept();
-        InputStream inputStream = socket.getInputStream();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-        while (bufferedReader.ready()) {
-            System.out.println(bufferedReader.readLine());
-        }
-        OutputStream outputStream =  socket.getOutputStream();
-        outputStream.write("HelloWold".getBytes());
-
-
-
-        System.out.println("Accepted connection from " + socket.getRemoteSocketAddress());
-        socket.close();
-        server.close();
+        handleHTTPRequest(socket);
     }
 
 
+    private static void handleHTTPRequest(Socket clientSocket) throws IOException {
+        try (
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)
+        ) {
+            String requestLine = in.readLine();
+            if (requestLine != null) {
 
+                String[] requestParts = requestLine.split(" ");
+                String method = requestParts[0];
+                String path = requestParts[1];
+
+                System.out.println("Received request: " + method + " " + path);
+
+
+            }
+
+            while (!requestLine.isBlank()) {
+                requestLine = in.readLine();
+                if (requestLine.equals("Server: True")) {
+                    System.out.println("server");
+                    break;
+                }
+
+                if (requestLine.equals("Server: False")) {
+                    System.out.println("client");
+                    break;
+                }
+            }
+
+            String response = "HTTP/1.1 200 OK\r\n" +
+                    "Content-Type: text/html\r\n"+
+                    "\r\n" +
+                    "FTP opening: " +
+                    requestLine;
+
+
+            out.println(response);
+
+        } finally {
+            clientSocket.close();
+        }
+
+
+    }
 }
